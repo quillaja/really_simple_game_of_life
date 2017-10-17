897f1f998dfa28b741f85eb940e6993672b11178
ben <quillaja@hotmail.com>
2017-10-16 23:52:06 -0700

    added methods for display

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 # as seen on https://www.youtube.com/watch?v=o9pEzgHorH0 @ 19:30
 
 import itertools
+import time
 
 
 def neighbors(point: (int, int)) -> (int, int):
@@ -28,5 +29,35 @@ def advance(board: set) -> set:
     return next_board
 
 
-INITIAL_GLIDER = {(0, 0), (1, 0), (2, 0), (0, 1), (1, 2)}
-TWO_X_TWO = {(1, 0), (2, 0), (1, 1), (2, 1)}
+def show(board: set) -> None:
+    '''display a grid of the board'''
+    get = lambda i, p: p[i]
+    get_x = lambda p: get(0, p)
+    get_y = lambda p: get(1, p)
+
+    high = max(itertools.chain(*board))
+    low = min(itertools.chain(*board))
+
+    for y in range(low - 1, high + 2):
+        for x in range(low - 1, high + 2):
+            if (x, y) in board:
+                print('#', end='')
+            else:
+                print('.', end='')
+        print(' {}'.format(y))
+
+    print('')  # blank line
+
+
+def animate(board: set, iterations: int, pause: float=0.5) -> None:
+    b = board.copy()
+    for i in range(iterations):
+        show(b)
+        b = advance(b)
+        time.sleep(pause)
+
+
+GLIDER = {(0, 0), (1, 0), (2, 0), (0, 1), (1, 2)}
+TWO_BY_TWO = {(1, 0), (2, 0), (1, 1), (2, 1)}
+BLINKER = {(0, 1), (1, 1), (2, 1)}
+TOAD = {(1, 0), (2, 0), (3, 0), (0, 1), (1, 1), (2, 1)}
